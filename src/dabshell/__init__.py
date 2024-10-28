0f4327d30f8729b4e4897db724b857fe155c7959
Damian Brunold <dab@dabsoft.ch>
2024-10-28 10:47:00 +0100

    Add comment re better command splitting

--- src/dabshell/__init__.py
+++ src/dabshell/__init__.py
@@ -950,7 +950,7 @@ class Dabshell:
         self.info_pythonproj_cwd = None
         self.info_git_cwd = None
         self.info_venv_cwd = None
-        cmds = line.split(" && ")
+        cmds = line.split(" && ")  # TODO only split if && is not in quoted arg
         for cmd_args in cmds:
             cmd, args = split_command(cmd_args, self)
             try:
